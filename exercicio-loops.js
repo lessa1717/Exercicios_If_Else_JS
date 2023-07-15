8726428d098939481989e5157ebc6bf86d944ed0
LucasLessa <lessa07oliveira@gmail.com>
2023-07-14 21:53:46 -0300

    exercicios de loops

---
+++ exercicio-loops.js
@@ -0,0 +1,69 @@
+// let prompt = require('prompt-sync')()
+
+
+// let nomes= ["Alana","Aléxia","Samanta","Thiago","Odirlei"]
+// //------------0        1       2         3           4
+
+// for (let contador = 0; contador < nomes.length; contador++) {
+//     const cadaElemento = nomes[contador];
+//     console.log(cadaElemento)
+// }
+
+// nomes.forEach(cadaElemento => {
+//     console.log(cadaElemento); 
+// })
+
+
+// const frutas = ["Pera","Uva", "Maca","Salada Mista"]
+
+// for (let contador = 0; contador < frutas.length; contador++) {
+//     const cadaElemento = frutas[contador];
+//     console.log(contador ,cadaElemento);
+
+// }
+
+// frutas.forEach(cadaElemento =>{
+//     console.log(cadaElemento;
+// })
+
+
+// let salario = 5000;
+// let contador = 0
+
+// while (salario < 5000) {
+//     salario += 100;
+//     contador++;
+
+//     console.log("O seu salario ainda é R$: " + salario);
+// }
+
+// console.log(contador);
+
+
+// do {
+//     salario +=100;
+//     console.log("O seu salario ainda é R$: "+ salario);
+// } while (salario < 5000);
+
+
+
+const prompt = require(`prompt-sync`)()
+
+let numeroEsolhido;
+// do {
+//     numeroEsolhido = prompt("Digite um numero: ");
+
+// } while(numeroEsolhido != "5");
+
+// while (numeroEsolhido != "5") {
+//     numeroEsolhido = prompt("Digite um numero: ")
+// }
+
+let senha;
+do {
+    senha = prompt("Digite a senha: ")
+    if (senha != 123) {
+        console.log("senha incorreta");
+    }
+    
+} while (senha != "123");
