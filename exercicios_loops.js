b7a166aae7e6d937d2cde67305e6db9ce5f0c132
LucasLessa <lessa07oliveira@gmail.com>
2023-07-17 21:47:12 -0300

    adicionado exercicio JS

---
+++ exercicios_loops.js
@@ -0,0 +1,112 @@
+const prompt = require(`prompt-sync`)()
+
+
+// Exercicio 01
+
+// for (let contador = 0; contador <=10 ; contador++) {
+//     console.log(contador)
+// }
+
+
+// Exercicio 02
+
+// for(let pares=0; pares<=10; ++pares){  
+
+//     if(pares%2==0)  
+
+//         console.log(pares)  
+// }
+
+// Exercicio 03
+
+// for(let impares=0; impares<=50; ++impares){  
+
+//         if(impares%2!=0)  
+    
+//             console.log(impares)  
+//     }
+    
+// Exercicio 04
+
+// let numero = parseInt(prompt("Digite um numero de 0 a 10: "))
+
+
+// for(let tabuada=1; tabuada<=10 ; tabuada++)
+// console.log(numero, "x"+tabuada+" = " + (numero*tabuada) )
+
+// let termo = parseInt(document.getElementById('numero').value);
+// var resposta = document.getElementById('resposta');
+// var penultimo=0, ultimo=1;
+// var numero;
+
+
+ //Exercicio - 05
+// let penultimo = 0;
+// let ultimo = 1;
+// let numero;
+
+// for(let fibonacci=0 ; fibonacci<=10 ; fibonacci++){
+//     numero = ultimo + penultimo;
+//     penultimo = ultimo;
+//     ultimo = numero;
+//     console.log(numero);
+// }
+
+//Exercicio 06
+
+// let  numeroPar =0 ;
+
+// while (numeroPar<=20) {
+
+//         console.log(numeroPar);
+//         numeroPar +=2;
+// }
+
+//Exercicio 07
+
+// let numero1 = 0 ;
+// let numero2 = 1;
+
+// while (numero2<=100) {
+
+//     numero1 += numero2
+
+//     numero2++
+//     console.log(numero1);
+
+// }
+
+
+
+
+
+//Exercicio 08
+
+// let idade = parseInt(prompt("Digite sua Idade: "))
+
+// while (idade<18) {
+//     console.log("Por favor digite novamente sua idade!");
+//     idade = parseInt(prompt("Digite sua Idade: "));
+// }
+// console.log("Voce é maior de idade!");
+
+
+//Exercicio 09
+
+// let numero = parseInt(prompt("Digite um numero: "))
+// while (numero!=-1) {
+// console.log(numero--)
+// } console.log("A contagem regressiva foi finalizada");
+
+
+//Exercicio 10
+
+// let numero1;
+
+// do {
+    
+//     numero1 = parseInt(prompt("Digite um numero: "))
+
+// } while (numero1 != 0);
+
+
